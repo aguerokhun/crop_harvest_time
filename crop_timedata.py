714360c4c6dbb07b17ec1a05668e2f68d9f1905e
aguerokhun <sanusitaiwo10@gmail.com>
2024-05-08 13:24:21 +0100

    Add files via upload

---
+++ crop_timedata.py
@@ -0,0 +1,31 @@
+import csv
+from fastapi import FastAPI, HTTPException
+from datetime import datetime, timedelta
+import pandas as pd
+
+with open("CROP PREDICTION.txt" , 'r') as txt_file:
+    lines = txt_file.readlines()
+with open('output.csv', 'w', newline= '') as csv_file:
+    writer = csv.writer(csv_file)
+for line in lines:
+    writer.writerow([line.strip()])
+    
+app = FastAPI()
+df = pd.read_csv("output.csv")
+
+@app.get("harvest_date")
+async def get_harvest_date(crop: str, date: str):
+    try:
+        planting_date = datetime.strptime(planting_date, "%Y-%m-%d")
+    except ValueError:
+        raise HTTPException(status_code= 400, detail = "Invalid Date Format, Use YYYY-MM-DD.")
+    
+    if crop not in df['CROP']:
+        raise HTTPException(status_code= 404, detail = "Crop not found")
+    
+    days_to_add = df.loc[df['CROP'] == crop, "DAYS"].values[0]
+    
+    harvest_date = planting_date + timedelta(days=days_to_add)
+    
+    return {"crop":crop, "planting_date" : planting_date.strftime("%y-%m-%d"), "harvest_date" : harvest_date.strftime("%y-%m-%d")}   
+    
